006b999808044ccdba835fac5ecc747ff8edaf74
minu kim <kmw106933@naver.com>
2022-04-05 02:42:54 +0900

    [Refactor] gitIgnore 적용

--- src/main/java/com/backend/moamoa/domain/post/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.backend.moamoa.domain.post.repository;
-
-import com.backend.moamoa.domain.post.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long>, PostCustomRepository {
-}
